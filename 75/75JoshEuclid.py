01c866ac086aae4383c8789ae12a12587960c491
Patrick de Mars <pwdemars@gmail.com>
2020-03-28 12:25:34 +0000

    added time and print answer

--- 75/75JoshEuclid.py
+++ 75/75JoshEuclid.py
@@ -4,18 +4,19 @@
 Created on Sat Mar 28 10:23:04 2020
 
 @author: joshuajacob
+
+Singular integer right triangles.
 """
 import numpy
 import math 
+import time
+
+s = time.time()
+
 l = 1500000
-
-
-
 lengths = numpy.zeros(l+1, dtype=numpy.int8)
 
-
 m = 2
-
 
 while m <= l/4:
     n = m%2 + 1
@@ -26,5 +27,6 @@ while m <= l/4:
         n += 2
     m += 1
         
-print(numpy.count_nonzero(lengths == 1))
-
+answer = numpy.count_nonzero(lengths == 1)
+print("Answer: {}".format(answer))
+print("Time taken: {:.2f}s".format(time.time()-s))
